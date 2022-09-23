29d259bd730ae1fa6ebc2339d313c10150fff5ea
Jonathan Etiz <ac13xero@gmail.com>
2022-09-23 10:45:15 -0700

    Dungeon Crawler

--- dungeon_crawl/dungeon_crawl.py
+++ dungeon_crawl/dungeon_crawl.py
@@ -76,31 +76,55 @@ class Character(Entity):
         for effect in self.active_effects:
             effect.do_effect(self)
 
+    # Simple check if this Character has an effect; returns the Effect object or False if it's not found.
+    def has_effect(self, effect_name):
+        for effect in self.active_effects:
+            if effect.name == effect_name:
+                return effect
+        return False
+
+    # Generic use the currently equipped item (use consumable or attack with weapon)
+    def use(self):
+        # If the equipped item is a weapon, get a target and pass, else just use the consumable.
+        if isinstance(self.equipped, Weapon):
+            self.get_target()
+            self.equipped.action(self, target)
+        else:
+            self.equipped.action(self)
+    
+    # Get a target; automatically selects the target with lowest health, or a specific target.
+    def get_target(self, take_input = False):
+        """Returns the Character object of the next available target; if self is the player character, it will prompt user for input. No input does auto-target."""
+        if take_input:
+            # Get input from user for tager
+        else:
+            # Return the first available target
+
 # Item that modifies an instigator's CharacterStatistics
 @dataclass
 class Consumable(Item):
     # CharacterStatistics, representing the effect (ie. -10 HP, +50 mana)
     affected_stats: CharacterStatistics
     # The effects this applies on the instigator
-    effects: dict[Effect]
+    effects: list[Effect]
     # Use this consumable
     def action(self, instigator: Character):
-        stats = instigator.stats
-        # For each of the affected_stats, if there are any affected_stats
-        if len(self.affected_stats) > 0:
-            for affected_stat in self.affected_stats:
-                # For each of the instigator's stats
-                for stat in stats:
-                    # If the affected_stat matches the instigator's stat
-                    if stat.name == affected_stat.name:
-                        # Apply the affected_stat's value to the instigator's stat
-                        stat.apply(affected_stat.value)
+
+        # For each attribute of affected_stats of Consumable, apply it to the instigator's stats
+        for attr, value in self.affected_stats.__dict__.items():
+            # Set instigator.stats.(attr) to instigator.stats.(attr) + value
+            setattr(instigator.stats, attr, getattr(instigator.stats, attr) + value)
 
         # For each of the effects, if there are any effects
         if len(self.effects) > 0:
             for effect in self.effects:
-                # Add the effect to 
-                instigator.active_effects.append(effect)
+                # Add the effect to the character
+                cur_effect = instigator.has_effect(effect.name)
+                # If the character already has the effect, reset duration, else add the new effect.
+                if cur_effect:
+                    cur_effect.duration = 0
+                else:
+                    instigator.active_effects.append(effect)
 
 
 @dataclass
@@ -116,7 +140,12 @@ class Weapon(Item):
         if self.can_attack(instigator):
             target.stats.hp -= self.effective_value()
             for effect in self.applied_effects:
-                target.active_effects.append(effect)
+                cur_effect = target.has_effect(effect.name)
+                # Only add the effect if the target doesn't have it, else reset duration
+                if cur_effect:
+                    cur_effect.duration = 0
+                else:
+                    target.active_effects.append(effect)
             return True
         else:
             return False
@@ -135,10 +164,18 @@ def main():
     # generate_dungeon
     # do stuff
     test_effect = Effect("Test Effect", 5, CharacterStatistics(10, -10, 0, 0))
-    test_char = Character("Jon", None, None, CharacterStatistics(500, 500, 250, 100), [test_effect])
+    test_consumable = Consumable("Test Potion", 5, CharacterStatistics(1000, 0, 0, -50), [test_effect])
+    test_char = Character("Jon", None, None, CharacterStatistics(500, 500, 250, 100), [])
+    print(test_char)
+    test_consumable.action(test_char)
+    print(test_char)
     while True:
         asd = input("Test?\n")
         if asd == "y":
+            test_char.tick()
+            print(test_char)
+        if asd == "n":
+            test_consumable.action(test_char)
             test_char.tick()
             print(test_char)
 
