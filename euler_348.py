67a7e608003c1584c404b035e41f8d4f5400763c
Jonathan Etiz <ac13xero@gmail.com>
2022-11-22 17:51:55 -0800

    Done. 58 +/- 3 second execution for 5 palindromes.

--- euler_348.py
+++ euler_348.py
@@ -1,7 +1,37 @@
 # Project Euler Problem 348
 # https://projecteuler.net/problem=348
 
+from time import time
+from etizmodules import input_int
+
 def is_palindrome(num):
-    return True if int(num) == int(str(num)[::-1]) else False
+    return num == int(str(num)[::-1])
     
-print(is_palindrome(1331))
+desired = input_int("How many palindromes do you want the sum of?", 1)
+found = {}
+found2 = {}
+
+start = time()
+for i in range(1,100000):
+    if len(found2) == desired:
+        break
+    for j in range(1,1000):
+        if len(found2) == desired:
+            break
+        val = i**2 + j**3
+        if is_palindrome(val):
+            if val in found.keys():
+                found[val].append((i, j))
+                if len(found[val]) == 4:
+                    found2[val] = found[val]
+            else:
+                found[val] = [(i, j)]
+end = time()
+
+output = 0
+
+for val in found2:
+    output += val
+
+print(output)
+print(f"{end - start} seconds elapsed.")
