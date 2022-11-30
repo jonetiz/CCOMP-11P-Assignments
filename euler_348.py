3945f4452343d8b7b06925998859ad62daef064f
Jonathan Etiz <ac13xero@gmail.com>
2022-11-29 16:16:46 -0800

    asdf commit notes etc

--- euler_348.py
+++ euler_348.py
@@ -15,10 +15,11 @@ start = time()
 for i in range(1,100000):
     if len(found2) == desired:
         break
+    i_squared = i**2
     for j in range(1,1000):
         if len(found2) == desired:
             break
-        val = i**2 + j**3
+        val = i_squared + j**3
         if is_palindrome(val):
             if val in found.keys():
                 found[val].append((i, j))
